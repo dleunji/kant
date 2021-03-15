2bc54fc82a60be7ef317e47560bbf9baf7d42e22
eunji <lej970703@naver.com>
2021-03-15 18:26:17 +0900

    env with docker

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask
-app = Flask(__name__)
+from transformers import AutoTokenizer
+import json
+import os
+
+app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/')
