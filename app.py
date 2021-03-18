5fee24e31c67e2d57535b99e3dc35143ce9215d8
eunji <lej970703@naver.com>
2021-03-19 00:11:05 +0900

    make index prettier

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
-from transformers import AutoTokenizer
+from flask import Flask, request, render_template, jsonify, Response
+"""from transformers import AutoTokenizer"""
+import requests
+import gpt_2_simple as gpt2
 import json
 import os
-
 app = Flask(__name__, static_url_path='/static')
+
+model_name = "/checkpoint/run1"
+sess = gpt2.start_tf_sess()
 
 
 @app.route('/')
-def hello_world():
-    return 'hello, Docker'
+def main():
+    return render_template("index.html")
+
+
+# @app.route('/gpt2', methods=['POST'])
+# def gpt2():
