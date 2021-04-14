4a36684edfd87eb5515fd22e8ba89897f9d10c92
dleunji <lej970703@naver.com>
2021-04-14 14:44:25 +0900

    modified swagger

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 from flask import Flask, request, render_template, jsonify, Response
 import requests
-#import gpt_2_simple as gpt2
 import json
 import os
 from transformers import AutoTokenizer
 
 autoTokenizer = AutoTokenizer.from_pretrained("gpt2-large")
-# os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 app = Flask(__name__, static_url_path='/static')
-# sess = gpt2.start_tf_sess()
-# gpt2.load_gpt2(sess)
-
 
 @app.route('/gpt2', methods=['POST'])
 def gpt2():
@@ -21,7 +16,7 @@ def gpt2():
     headers = {'Content-Type' : 'application/json; charset=utf-8'}
     data = {
         'text' : encodedText,
-        'length' : 200,
+        'length' : 170,
         'num_samples' : 1
     }
     url = "https://train-avgw7n5kbmsb7wrip2a8-gpt2-train-teachable-ainize.endpoint.dev.ainize.ai/predictions/gpt-2-en-small-finetune"
