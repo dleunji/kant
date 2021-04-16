7957fdff3a7447f6f74ff1fc6a91920ea015003c
dleunji <lej970703@naver.com>
2021-04-16 17:26:18 +0900

    add enter key

--- app.py
+++ app.py
@@ -16,13 +16,13 @@ def gpt2():
     headers = {'Content-Type' : 'application/json; charset=utf-8'}
     data = {
         'text' : encodedText,
-        'length' : 170,
-        'num_samples' : 1
+        'length' : 200
     }
     url = "https://train-avgw7n5kbmsb7wrip2a8-gpt2-train-teachable-ainize.endpoint.dev.ainize.ai/predictions/gpt-2-en-small-finetune"
     response = requests.post(url, headers = headers, data = json.dumps(data))
     if response.status_code == 200:
         res = response.json()
+        print(res[0])
         text = autoTokenizer.decode(res[0], skip_special_tokens=True)
         print(text)
         return jsonify({'text' : text}), 200
